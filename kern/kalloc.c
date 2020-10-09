b7d046dc95fa2d3ce944eb5bc15c7d66541be677
Tan-YiFan <15711657500@163.com>
2020-10-09 16:49:54 +0800

    modify kalloc

--- kern/kalloc.c
+++ kern/kalloc.c
@@ -42,28 +42,31 @@ kfree(char* v)
     memset(v, 1, PGSIZE);
 
     /* TODO: Your code here. */
-    if (kmem.free_list == NULL) {
-        kmem.free_list = (struct run*)v;
-        kmem.free_list->next = NULL;
-    }
-    else {
-        struct run* k;
-        k = kmem.free_list;
-        kmem.free_list = (struct run*)v;
-        kmem.free_list->next = k;
-        // for (r = kmem.free_list; r; r = r->next) {
-        //     // the page has already been free
-        //     if (r == (struct run*)v) {
-        //         break;
-        //     }
+    r = (struct run*)v;
+    r->next = kmem.free_list;
+    kmem.free_list = r;
+    // if (kmem.free_list == NULL) {
+    //     kmem.free_list = (struct run*)v;
+    //     kmem.free_list->next = NULL;
+    // }
+    // else {
+    //     struct run* k;
+    //     k = kmem.free_list;
+    //     kmem.free_list = (struct run*)v;
+    //     kmem.free_list->next = k;
+    //     // for (r = kmem.free_list; r; r = r->next) {
+    //     //     // the page has already been free
+    //     //     if (r == (struct run*)v) {
+    //     //         break;
+    //     //     }
 
-        //     if (r->next == NULL) {
-        //         r->next = (struct run*)v;
-        //         r->next->next = NULL;
-        //         break;
-        //     }
-        // }
-    }
+    //     //     if (r->next == NULL) {
+    //     //         r->next = (struct run*)v;
+    //     //         r->next->next = NULL;
+    //     //         break;
+    //     //     }
+    //     // }
+    // }
 
 
 }
@@ -86,10 +89,11 @@ char*
 kalloc()
 {
     /* TODO: Your code here. */
+    char* ret = (char*)kmem.free_list;
     if (kmem.free_list == NULL) {
         return 0;
     }
-    char* ret = (char*)kmem.free_list;
+
     kmem.free_list = kmem.free_list->next;
 
     return ret;
