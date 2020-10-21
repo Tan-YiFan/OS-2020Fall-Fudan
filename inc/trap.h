9f8241d85364add4801089433a0e9f1b67ee68de
Tan-YiFan <15711657500@163.com>
2020-10-21 20:12:29 +0800

    add trapframe

--- inc/trap.h
+++ inc/trap.h
@@ -5,9 +5,13 @@
 
 struct trapframe {
     /* TODO: Design your own trapframe layout here. */
+    uint64_t elr_el1;
+    uint64_t spsr_el1;
+    uint64_t sp_el0;
+    uint64_t r[31];
 };
 
-void trap(struct trapframe *);
+void trap(struct trapframe*);
 void irq_init();
 void irq_error();
 
